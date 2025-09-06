4cdf7765afb2b65aefce672ad10810f51aa01fd5
Korbinian <korbinian.kolb@sap.com>
2025-09-06 11:52:15 +0200

    ...

--- rss_mail_summarizer/utils/create_gmail_token_locally.py
+++ rss_mail_summarizer/utils/create_gmail_token_locally.py
@@ -3,12 +3,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
 flow = InstalledAppFlow.from_client_secrets_file(
-    "credentials/credentials.json", SCOPES
+    "rss_mail_summarizer/credentials/credentials.json", SCOPES
 )
 creds = flow.run_local_server(port=0)
 
 # Token speichern
-with open("credentials/token.json", "w") as token:
+with open("rss_mail_summarizer/credentials/token.json", "w") as token:
     token.write(creds.to_json())
 
 print("Token erfolgreich gespeichert.")
