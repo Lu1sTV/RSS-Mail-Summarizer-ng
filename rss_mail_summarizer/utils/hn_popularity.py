3247246436d4ed9c72d9b73f334d0b497e28bbf8
SebastianWolfP <sebastian.wolf1@porsche.de>
2025-09-06 13:16:54 +0200

    delete extract_from_rss and done comments for hn_popularity

--- rss_mail_summarizer/utils/hn_popularity.py
+++ rss_mail_summarizer/utils/hn_popularity.py
@@ -1,5 +1,12 @@
+""" Dieses Modul dient dazu, die Popularität eines Artikels auf Hacker News zu ermitteln. 
+Dazu wird die Algolia-API von Hacker News abgefragt, welche Stories auf Hacker News anhand der URL durchsucht. 
+Die Funktion gibt die Anzahl der Points für die gefundene Story zurück, 
+oder None, falls keine Ergebnisse gefunden werden oder ein Fehler auftritt. """
+
 import requests
 
+""" Die Function ruft die Anzahl der Punkte eines Artikels auf Hacker News ab. Als Parameter wird die URL des gesuchten Artikels übergeben.
+Als maximale Wartezeit für die Anfrage können optional Sekunden angegeben werden. """
 def fetch_hn_points(url: str, timeout=8):
     try:
         r = requests.get(
